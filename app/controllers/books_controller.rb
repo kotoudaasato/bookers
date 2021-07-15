34f16bda2a861a59dd7f4dd8cc73e3bb0ac8eb14
kotoudaasato <aru.cam.wanwan@gmail.com>
2021-07-15 09:38:41 +0000

    [Finsh]bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,17 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
+    @book = Book.new
   end
   def create
     @book = Book.new(book_params)
-    @book.save
-    redirect_to book_path(@book.id)
-
+    if @book.save
+      flash[:new] = 'Book was successfully created.'
+      redirect_to book_path(@book)
+    else
+      @books = Book.all
+      render:index
+    end
   end
 
   def show
@@ -18,9 +23,19 @@ class BooksController < ApplicationController
   end
 
   def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:update] = 'Book was successfully update.'
+      redirect_to book_path(@book.id)
+    else
+      render:edit
+    end
+  end
+
+  def destroy
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    book.destroy
+    redirect_to books_path
   end
 
   private
